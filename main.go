e10426d444ade43c78b5e7c5329e2f5e406f0e11
mzmt <mrcrius1355@gmail.com>
2020-06-12 17:46:58 +0900

    diary api

--- main.go
+++ main.go
@@ -4,18 +4,36 @@ import (
     "fmt"
     "log"
     "net/http"
+    "encoding/json"
+    // "github.com/mzmt/golang_api_sample/handler"
+    // "github.com/mzmt/golang_api_sample/model"
 )
 
-func topPage(w http.ResponseWriter, r *http.Request) {
+type Diary struct {
+    Content string `json:content`
+}
+var Diaries []Diary
+
+func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "welcome")
     fmt.Println("server start")
 }
 
+func returnAllDiaries(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllDiaries")
+    json.NewEncoder(w).Encode(Diaries)
+}
+
 func handleRequests() {
     http.HandleFunc("/", homePage)
+    http.HandleFunc("/diaries", returnAllDiaries)
     log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+    Diaries = []Diary {
+        Diary{ Content: "It was raining today."},
+        Diary{ Content: "It was cloudy today."},
+    }
     handleRequests()
 }
